a18a618d8f789c35b8e10c4f791e96851641796e
weonest <geonhee33@naver.com>
2024-01-25 16:39:03 +0900

    feat : 에러 응답 객체 추가 및 글로벌 예외 핸들러 추가

---
+++ src/main/java/kyonggiyo/global/advice/GlobalExceptionHandler.java
@@ -0,0 +1,77 @@
+package kyonggiyo.global.advice;
+
+import kyonggiyo.global.exception.AuthenticationException;
+import kyonggiyo.global.exception.ErrorCode;
+import kyonggiyo.global.exception.ForbiddenException;
+import kyonggiyo.global.exception.GlobalErrorCode;
+import kyonggiyo.global.exception.InvalidValueException;
+import kyonggiyo.global.exception.NotFoundException;
+import kyonggiyo.global.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(Exception e) {
+        log.warn("예상치 못한 서버 예외가 발생하였습니다.", e);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(InvalidValueException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        log.warn(exception.getLoggingMessage(), exception);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(exception.getErrorCode()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(AuthenticationException exception) {
+        log.warn(exception.getLoggingMessage(), exception);
+        return ResponseEntity.status(UNAUTHORIZED)
+                .body(ErrorResponse.of(exception.getErrorCode()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(ForbiddenException exception) {
+        log.warn(exception.getLoggingMessage(), exception);
+        return ResponseEntity.status(FORBIDDEN)
+                .body(ErrorResponse.of(exception.getErrorCode()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(NotFoundException exception) {
+        log.warn(exception.getLoggingMessage(), exception);
+        return ResponseEntity.status(NOT_FOUND)
+                .body(ErrorResponse.of(exception.getErrorCode()));
+    }
+
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(MethodArgumentNotValidException exception) {
+        log.warn(exception.getMessage(), exception.getBindingResult());
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(GlobalErrorCode.INPUT_VALIDATION_ERROR, exception.getBindingResult()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(MethodArgumentTypeMismatchException exception) {
+        log.warn(exception.getMessage(), exception);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(exception));
+    }
+
+}
