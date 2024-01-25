f7ae07ee8349926670204aad69dd8418463ca197
weonest <geonhee33@naver.com>
2024-01-25 16:05:35 +0900

    feat : 인증 Url 생성 해결 + 컨버터 설정 추가 - 객체 생성 과정이 아닌 .env 파일에서 문제 발생인듯

--- src/main/java/kyonggiyo/application/service/oauth/KakaoAuthCodeRequestUrlProvider.java
+++ src/main/java/kyonggiyo/application/service/oauth/KakaoAuthCodeRequestUrlProvider.java
@@ -11,19 +11,18 @@ import java.net.URI;
 
 @Getter
 @Component
-@RequiredArgsConstructor
-public class KakaoAuthCodeRequestUrlProvider {
+public class KakaoAuthCodeRequestUrlProvider extends AuthCodeRequestUrlProvider{
 
     private static final String AUTHORIZATION_URL = "https://kauth.kakao.com/oauth/authorize";
 
     private final KakaoOAuthProperties kakaoOAuthProperties;
 
-//    public KakaoAuthCodeRequestUrlProvider(KakaoOAuthProperties kakaoOAuthProperties) {
-////        super(Platform.KAKAO);
-//        this.kakaoOAuthProperties = kakaoOAuthProperties;
-//    }
+    public KakaoAuthCodeRequestUrlProvider(KakaoOAuthProperties kakaoOAuthProperties) {
+        super(Platform.KAKAO);
+        this.kakaoOAuthProperties = kakaoOAuthProperties;
+    }
 
-//    @Override
+    @Override
     public URI provideUri() {
         return UriComponentsBuilder
                 .fromUriString(AUTHORIZATION_URL)
---
+++ src/main/java/kyonggiyo/global/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package kyonggiyo.global.config;
+
+import kyonggiyo.util.PlatformConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new PlatformConverter());
+    }
+
+}
