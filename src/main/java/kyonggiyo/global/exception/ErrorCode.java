ab513371624ea60df9b2ec66f9c5b3bb9cfa6870
weonest <geonhee33@naver.com>
2024-01-25 16:30:12 +0900

    feat : 사용자 정의 예외 코드 및 예외 클래스 추가

---
+++ src/main/java/kyonggiyo/global/exception/ErrorCode.java
@@ -0,0 +1,10 @@
+package kyonggiyo.global.exception;
+
+
+public interface ErrorCode {
+
+    String code();
+
+    String message();
+
+}
---
+++ src/main/java/kyonggiyo/global/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package kyonggiyo.global.exception;
+
+public class NotFoundException extends BusinessException {
+
+    public NotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public NotFoundException(ErrorCode errorCode, String loggingMessage) {
+        super(errorCode, loggingMessage);
+    }
+
+}
