0aff95edff0f96bfdbbfc30f89f5d4fd1ee5e245
weonest <geonhee33@naver.com>
2024-01-26 03:11:26 +0900

    config : 오픈페인 설정 추가

---
+++ src/main/java/kyonggiyo/global/config/OpenFeignConfig.java
@@ -0,0 +1,9 @@
+package kyonggiyo.global.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients("kyonggiyo.adapter.out.client.oauth")
+public class OpenFeignConfig {
+}
