d451f643cb398413f38df9c9c1c65b30278e5782
weonest <geonhee33@naver.com>
2024-01-29 17:22:12 +0900

    config : Jwt 관련 환경변수 추가

--- src/main/java/kyonggiyo/global/config/PropertiesConfig.java
+++ src/main/java/kyonggiyo/global/config/PropertiesConfig.java
@@ -1,11 +1,15 @@
 package kyonggiyo.global.config;
 
+import kyonggiyo.global.property.JwtProperties;
 import kyonggiyo.global.property.KakaoOAuthProperties;
 import kyonggiyo.global.property.NaverOAuthProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(value = {KakaoOAuthProperties.class, NaverOAuthProperties.class})
+@EnableConfigurationProperties(value = {
+        KakaoOAuthProperties.class,
+        NaverOAuthProperties.class,
+        JwtProperties.class})
 public class PropertiesConfig {
 }
---
+++ src/main/java/kyonggiyo/global/property/JwtProperties.java
@@ -0,0 +1,13 @@
+package kyonggiyo.global.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+        String claimId,
+        String claimRole,
+        String secretKey,
+        long accessTokenExpireTime,
+        long refreshTokenExpireTime
+) {
+}
