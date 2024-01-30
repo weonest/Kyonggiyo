9a3ba1d9f7751a69c094eaa31e0e8f10e41fae5c
weonest <geonhee33@naver.com>
2024-01-30 20:42:27 +0900

    style : 글로벌 예외 코드 수정

--- src/main/java/kyonggiyo/global/exception/GlobalErrorCode.java
+++ src/main/java/kyonggiyo/global/exception/GlobalErrorCode.java
@@ -3,7 +3,7 @@ package kyonggiyo.global.exception;
 public enum GlobalErrorCode implements ErrorCode {
 
     INTERNAL_SERVER_ERROR("G001", "서버에 알 수 없는 문제가 발생했습니다."),
-    INPUT_VALIDATION_ERROR("G002", "요청 정보가 유효하지 않습니다.");
+    INVALID_REQUEST_ERROR("G002", "요청 정보가 유효하지 않습니다.");
 
     private final String code;
     private final String message;
