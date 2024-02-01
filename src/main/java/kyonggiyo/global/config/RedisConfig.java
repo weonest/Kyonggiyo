2e96491b44361782eb2d12372017ca7a7e3ed4e5
weonest <geonhee33@naver.com>
2024-02-01 23:51:24 +0900

    feat : key가 아닌 value로 조회하기 위해 RedisRepository 활성화

--- src/main/java/kyonggiyo/global/config/RedisConfig.java
+++ src/main/java/kyonggiyo/global/config/RedisConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
     @Bean
