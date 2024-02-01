86c00b9c4de98886763b8171abbf297094701e8e
weonest <geonhee33@naver.com>
2024-02-01 23:52:36 +0900

    feat : RedisTemplate -> RedisRepository에 따른 RefreshToken 객체 수정

--- src/main/java/kyonggiyo/domain/auth/RefreshToken.java
+++ src/main/java/kyonggiyo/domain/auth/RefreshToken.java
@@ -1,7 +1,32 @@
 package kyonggiyo.domain.auth;
 
-public record RefreshToken(
-        String value,
-        long maxAge
-){
+import kyonggiyo.domain.user.Role;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@RedisHash(timeToLive = 60)
+public class RefreshToken {
+
+    @Id
+    private Long userId;
+
+    private Role role;
+
+    @Indexed
+    private String value;
+
+    private long expiresIn;
+
+    @Builder
+    private RefreshToken(Long userId, String value, Role role, long expiresIn) {
+        this.userId = userId;
+        this.value = value;
+        this.role = role;
+        this.expiresIn = expiresIn;
+    }
+
 }
