72317a45954e95d8c2e1c24b6697428457ec91fd
weonest <geonhee33@naver.com>
2024-02-26 17:29:15 +0900

    refactor: 식당 쿼리 서비스 수정

--- src/main/java/kyonggiyo/application/service/restaurant/RestaurantQueryService.java
+++ src/main/java/kyonggiyo/application/service/restaurant/RestaurantQueryService.java
@@ -1,11 +1,15 @@
 package kyonggiyo.application.service.restaurant;
 
+import kyonggiyo.adapter.in.web.restaurant.dto.RestaurantMarkerResponse;
+import kyonggiyo.adapter.in.web.restaurant.dto.RestaurantResponse;
 import kyonggiyo.application.port.in.restaurant.GetRestaurantQuery;
 import kyonggiyo.application.port.out.restaurant.GetRestaurantPort;
 import kyonggiyo.domain.restaurant.Restaurant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +19,15 @@ public class RestaurantQueryService implements GetRestaurantQuery {
     private final GetRestaurantPort getRestaurantPort;
 
     @Override
-    public Restaurant get(Long id) {
-        return getRestaurantPort.getById(id);
+    public RestaurantResponse getById(Long id) {
+        return RestaurantResponse.from(getRestaurantPort.getById(id));
+    }
+
+    @Override
+    public List<RestaurantMarkerResponse> getAllRestaurantsForMarker() {
+        List<Restaurant> restaurants = getRestaurantPort.getAllRestaurants();
+        return restaurants.stream()
+                .map(RestaurantMarkerResponse::from).toList();
     }
 
 }
