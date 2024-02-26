ceb44447013874cba6f420c6d9449a8d4b18c16c
weonest <geonhee33@naver.com>
2024-02-26 16:24:33 +0900

    annotation : todo

--- src/main/java/kyonggiyo/application/service/auth/AccountLoginService.java
+++ src/main/java/kyonggiyo/application/service/auth/AccountLoginService.java
@@ -13,11 +13,12 @@ public class AccountLoginService {
 
     private final FindAccountPort findAccountPort;
     private final AccountSignUpService accountSignUpService;
-
+    
     @Transactional(readOnly = true)
     public Account login(Platform platform, String platformId) {
         return findAccountPort.findByPlatformAndPlatformId(platform, platformId)
                 .orElseGet(() -> accountSignUpService.signup(platform, platformId));
     }
+    // TODO: 2024-02-26 H2 에서는 readOnly 설정이 무시됨으로 MySQL 사용시 오류 발생할 수도 있음
 
 }
