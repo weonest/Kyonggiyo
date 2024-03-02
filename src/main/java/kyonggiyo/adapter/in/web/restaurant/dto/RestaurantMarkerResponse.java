664a4925340b65ea981e7e321636264685cc3882
weonest <geonhee33@naver.com>
2024-03-02 17:03:37 +0900

    feat: 레스토랑 마커 정보에 카테고리 추가

--- src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/RestaurantMarkerResponse.java
+++ src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/RestaurantMarkerResponse.java
@@ -1,11 +1,13 @@
 package kyonggiyo.adapter.in.web.restaurant.dto;
 
 import kyonggiyo.domain.restaurant.Restaurant;
+import kyonggiyo.domain.restaurant.RestaurantCategory;
 
 public record RestaurantMarkerResponse(
         Long id,
         String name,
         float averageRating,
+        RestaurantCategory category,
         double lat,
         double lng
 ) {
@@ -15,6 +17,7 @@ public record RestaurantMarkerResponse(
                 restaurant.getId(),
                 restaurant.getName(),
                 restaurant.getAverageRating(),
+                restaurant.getCategory(),
                 restaurant.getAddress().getLat(),
                 restaurant.getAddress().getLng()
         );
