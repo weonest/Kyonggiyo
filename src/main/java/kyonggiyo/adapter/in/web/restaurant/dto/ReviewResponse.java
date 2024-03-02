477f82a235d0a4db8ff114bfd210b73cccf82053
weonest <geonhee33@naver.com>
2024-03-02 13:10:22 +0900

    feat : 리뷰 응답 생성일 추가

--- src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/ReviewResponse.java
+++ src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/ReviewResponse.java
@@ -3,10 +3,13 @@ package kyonggiyo.adapter.in.web.restaurant.dto;
 import kyonggiyo.adapter.in.web.user.dto.UserResponse;
 import kyonggiyo.domain.restaurant.Review;
 
+import java.time.LocalDateTime;
+
 public record ReviewResponse(
         Long id,
         int rating,
         String content,
+        LocalDateTime createdAt,
         UserResponse reviewer
 ) {
 
@@ -15,6 +18,7 @@ public record ReviewResponse(
                 review.getId(),
                 review.getRating(),
                 review.getContent(),
+                review.getCreatedAt(),
                 new UserResponse(review.getReviewer().getId(),
                         review.getReviewer().getNickname())
         );
