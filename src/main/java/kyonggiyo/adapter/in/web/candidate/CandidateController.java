01a49caccb5e546958453cce6af48a5d291b09ca
weonest <geonhee33@naver.com>
2024-03-02 14:09:31 +0900

    feat : Candidate Controller & Service 삭제 기능 추가

--- src/main/java/kyonggiyo/adapter/in/web/candidate/CandidateController.java
+++ src/main/java/kyonggiyo/adapter/in/web/candidate/CandidateController.java
@@ -3,15 +3,19 @@ package kyonggiyo.adapter.in.web.candidate;
 import kyonggiyo.adapter.in.web.candidate.dto.CandidateCreateRequest;
 import kyonggiyo.adapter.in.web.candidate.dto.CandidateResponse;
 import kyonggiyo.application.port.in.candidate.CreateCandidateUseCase;
+import kyonggiyo.application.port.in.candidate.DeleteCandidateUseCase;
 import kyonggiyo.application.port.in.candidate.FindCandidateUseCase;
 import kyonggiyo.domain.candidate.Status;
+import kyonggiyo.global.auth.Admin;
 import kyonggiyo.global.auth.UserInfo;
 import kyonggiyo.global.response.SliceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,20 +31,28 @@ public class CandidateController {
 
     private final CreateCandidateUseCase createCandidateUseCase;
     private final FindCandidateUseCase findCandidateUseCase;
+    private final DeleteCandidateUseCase deleteCandidateUseCase;
 
     @PostMapping
-    public ResponseEntity<Void> createCandidate(UserInfo userInfo, @RequestBody CandidateCreateRequest request) {
+    public ResponseEntity<Void> candidateCreate(UserInfo userInfo, @RequestBody CandidateCreateRequest request) {
         createCandidateUseCase.createCandidate(userInfo, request);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ResponseEntity<SliceResponse> findAllCandidatesByStatus(UserInfo userInfo,
+    public ResponseEntity<SliceResponse<CandidateResponse>> candidateListByStatus(UserInfo userInfo,
                                                                    @RequestParam Status status,
                                                                    @RequestParam(required = false, defaultValue = "0", value = "page") int page) {
         Pageable pageable = PageRequest.of(page, DEFAULT_PAGE_SIZE);
         SliceResponse<CandidateResponse> response = findCandidateUseCase.findAllByStatus(userInfo, status, pageable);
         return ResponseEntity.ok(response);
+    }
+
+    @Admin
+    @DeleteMapping("/{candidateId}")
+    public ResponseEntity<Void> candidateDelete(UserInfo userInfo, @PathVariable Long candidateId) {
+        deleteCandidateUseCase.deleteCandidate(userInfo, candidateId);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/kyonggiyo/application/service/candidate/CandidateCommandService.java
+++ src/main/java/kyonggiyo/application/service/candidate/CandidateCommandService.java
@@ -2,9 +2,15 @@ package kyonggiyo.application.service.candidate;
 
 import kyonggiyo.adapter.in.web.candidate.dto.CandidateCreateRequest;
 import kyonggiyo.application.port.in.candidate.CreateCandidateUseCase;
+import kyonggiyo.application.port.in.candidate.DeleteCandidateUseCase;
+import kyonggiyo.application.port.out.candidate.DeleteCandidatePort;
+import kyonggiyo.application.port.out.candidate.FindCandidatePort;
 import kyonggiyo.application.port.out.candidate.SaveCandidatePort;
 import kyonggiyo.domain.candidate.Candidate;
+import kyonggiyo.domain.user.Role;
 import kyonggiyo.global.auth.UserInfo;
+import kyonggiyo.global.exception.ForbiddenException;
+import kyonggiyo.global.exception.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,14 +18,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class CandidateCommandService implements CreateCandidateUseCase {
+public class CandidateCommandService implements CreateCandidateUseCase, DeleteCandidateUseCase {
 
     private final SaveCandidatePort saveCandidatePort;
+    private final FindCandidatePort findCandidatePort;
+    private final DeleteCandidatePort deleteCandidatePort;
 
     @Override
     public void createCandidate(UserInfo userInfo, CandidateCreateRequest request) {
         Candidate candidate = request.toEntity(userInfo.userId());
         saveCandidatePort.save(candidate);
+    }
+
+    @Override
+    public void deleteCandidate(UserInfo userInfo, Long id) {
+        Candidate candidate = findCandidatePort.getById(id);
+        if (userInfo.role().equals(Role.ADMIN) || candidate.getRequesterId().equals(userInfo.userId())) {
+            deleteCandidatePort.deleteById(id);
+            return;
+        }
+        throw new ForbiddenException(GlobalErrorCode.INVALID_REQUEST_EXCEPTION,
+                String.format("유저 식별자 불일치 -> %d", userInfo.userId()));
     }
 
 }
