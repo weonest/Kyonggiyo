6356d3b62881a7ca5244ea72f1ae4a1db0dec16c
weonest <geonhee33@naver.com>
2024-03-03 20:58:13 +0900

    test: 식당 Adapter Test

--- src/test/java/kyonggiyo/adapter/out/persistence/restaurant/RestaurantPersistenceAdapterTest.java
+++ src/test/java/kyonggiyo/adapter/out/persistence/restaurant/RestaurantPersistenceAdapterTest.java
@@ -1,10 +1,15 @@
 package kyonggiyo.adapter.out.persistence.restaurant;
 
 import kyonggiyo.adapter.out.persistence.AdapterTest;
+import kyonggiyo.adapter.out.persistence.restaurant.review.ReviewRepository;
 import kyonggiyo.domain.restaurant.Restaurant;
+import kyonggiyo.domain.restaurant.Review;
 import kyonggiyo.fixture.RestaurantFixtures;
+import kyonggiyo.fixture.ReviewFixtures;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,21 +21,78 @@ class RestaurantPersistenceAdapterTest extends AdapterTest {
     @Autowired
     private RestaurantPersistenceAdapter restaurantPersistenceAdapter;
 
+    @Autowired
+    private ReviewRepository reviewRepository;
+
     @Test
     void 식별자를_통해_DB에서_Restaurant을_조회할_수_있다() {
         // given
-        Restaurant restaurant = RestaurantFixtures.generateRestaurantEntityWithoutReview();
-        restaurantRepository.save(restaurant);
-
+        Restaurant restaurant = RestaurantFixtures.generateRestaurantDomainWithoutReview();
+        Restaurant expect = restaurantRepository.save(restaurant);
 
         entityManager.flush();
         entityManager.clear();
 
         // when
-        Restaurant result = restaurantPersistenceAdapter.getById(restaurant.getId());
+        Restaurant result = restaurantPersistenceAdapter.getById(expect.getId());
 
         // then
-        assertThat(result).isEqualTo(result);
+        assertThat(result).isEqualTo(restaurant);
+    }
+
+    @Test
+    void DB에서_전체_Restaurant을_조회할_수_있다() {
+        // given
+        int expectSize = 2;
+        Restaurant restaurant = RestaurantFixtures.generateRestaurantDomainWithoutReview();
+        Restaurant restaurant2 = RestaurantFixtures.generateRestaurantDomainWithoutReview();
+        restaurantRepository.save(restaurant);
+        restaurantRepository.save(restaurant2);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        List<Restaurant> result = restaurantPersistenceAdapter.findAll();
+
+        // then
+        assertThat(result).hasSize(expectSize);
+    }
+
+    @Test
+    void DB에서_Restaurant을_이름_또는_리뷰내용으로_조회할_수_있다() {
+        // given
+        Restaurant restaurant = RestaurantFixtures.generateRestaurantDomainWithoutReview();
+        Review review = ReviewFixtures.createReviewDomain(restaurant);
+        restaurantRepository.save(restaurant);
+        reviewRepository.save(review);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        List<Restaurant> resultByName = restaurantPersistenceAdapter.findByNameOrReviewContent(restaurant.getName());
+        List<Restaurant> resultByReviewContent = restaurantPersistenceAdapter.findByNameOrReviewContent("떡볶이");
+
+        // then
+        assertThat(resultByName.get(0)).isEqualTo(restaurant);
+        assertThat(resultByReviewContent.get(0)).isEqualTo(restaurant);
+    }
+
+    @Test
+    void DB에서_Restaurant을_카테고리로_조회할_수_있다() {
+        // given
+        Restaurant restaurant = RestaurantFixtures.generateRestaurantDomainWithoutReview();
+        restaurantRepository.save(restaurant);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        List<Restaurant> resultByName = restaurantPersistenceAdapter.filterByCategory(List.of(restaurant.getCategory()));
+
+        // then
+        assertThat(resultByName.get(0)).isEqualTo(restaurant);
     }
 
 }
