feeee1947fa2b0c557602ff74672dfc087def644
weonest <geonhee33@naver.com>
2024-03-03 20:54:44 +0900

    test: 식당, 리뷰 Fixture 추가

--- src/test/java/kyonggiyo/fixture/RestaurantFixtures.java
+++ src/test/java/kyonggiyo/fixture/RestaurantFixtures.java
@@ -2,7 +2,10 @@ package kyonggiyo.fixture;
 
 
 import kyonggiyo.domain.restaurant.Restaurant;
+import kyonggiyo.domain.restaurant.RestaurantCategory;
 import org.instancio.Instancio;
+
+import java.util.List;
 
 import static org.instancio.Select.field;
 
@@ -11,9 +14,24 @@ public class RestaurantFixtures {
     private RestaurantFixtures() {
     }
 
-    public static Restaurant generateRestaurantEntityWithoutReview() {
+    public static Restaurant generateRestaurantDomainWithoutReview() {
         return Instancio.of(Restaurant.class)
+                .ignore(field(Restaurant::getId))
                 .ignore(field(Restaurant::getReviews))
+                .create();
+    }
+
+    public static List<Restaurant> generateRestaurantEntityList(String keyword, int targetSize) {
+        return Instancio.ofList(Restaurant.class)
+                .size(targetSize)
+                .generate(field(Restaurant::getName), gen -> gen.string().prefix(keyword))
+                .create();
+    }
+
+    public static List<Restaurant> generateRestaurantEntityList(RestaurantCategory category, int targetSize) {
+        return Instancio.ofList(Restaurant.class)
+                .size(targetSize)
+                .set(field(Restaurant::getCategory), category)
                 .create();
     }
 
