64366ea4e1ccdbd52a983bcf47b628b8098ab0ea
weonest <geonhee33@naver.com>
2024-03-03 21:23:50 +0900

    refactor: 식당 검색 필터링 Dto 수정

--- src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/RestaurantByKeywordRequest.java
+++ src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/RestaurantByKeywordRequest.java
@@ -1,9 +1,6 @@
 package kyonggiyo.adapter.in.web.restaurant.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record RestaurantByKeywordRequest (
-        @NotBlank
         String keyword
 ) {
 }
--- src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/RestaurantFilterRequest.java
+++ src/main/java/kyonggiyo/adapter/in/web/restaurant/dto/RestaurantFilterRequest.java
@@ -1,6 +1,9 @@
 package kyonggiyo.adapter.in.web.restaurant.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record RestaurantFilterRequest (
+        @NotBlank
         String categories
 ) {
 }
