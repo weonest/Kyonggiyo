af2a2030be6205f241a4531cb8f008d358f5522e
weonest <geonhee33@naver.com>
2024-03-04 20:43:27 +0900

    refactor: WebConfig allowedOrigins 프론트 요청 도메인으로 변경

--- src/main/java/kyonggiyo/global/config/WebConfig.java
+++ src/main/java/kyonggiyo/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 도메인 주소로
+                .allowedOriginPatterns("http://localhost:3000") // 도메인 주소로
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
