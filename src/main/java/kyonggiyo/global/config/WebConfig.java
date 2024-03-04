246868e47cc0732232704a5bff54de7271b9369b
weonest <geonhee33@naver.com>
2024-03-04 20:30:03 +0900

    refactor: WebConfig allowedOrigins로 변경

--- src/main/java/kyonggiyo/global/config/WebConfig.java
+++ src/main/java/kyonggiyo/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 도메인 주소로
+                .allowedOrigins("*") // 도메인 주소로
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
