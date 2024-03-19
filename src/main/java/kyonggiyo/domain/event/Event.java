2fc93a73d2e56c86efa899a06a85bcfd9178f840
weonest <geonhee33@naver.com>
2024-03-19 23:58:54 +0900

    :sparkles: Event 저장소를 위한 엔티티 생성

---
+++ src/main/java/kyonggiyo/domain/event/Event.java
@@ -0,0 +1,53 @@
+package kyonggiyo.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import kyonggiyo.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "events")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Event extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private EventType eventType;
+
+    @Enumerated(EnumType.STRING)
+    private EntityType entityType;
+
+    private Long entityId;
+
+    private String EntityData;
+
+    private boolean status = false;
+
+    @Builder
+    private Event(EventType eventType,
+                  EntityType entityType,
+                  Long entityId,
+                  String entityData) {
+        this.eventType = eventType;
+        this.entityType = entityType;
+        this.entityId = entityId;
+        EntityData = entityData;
+    }
+
+    public void successEvent() {
+        this.status = true;
+    }
+
+}
---
+++ src/main/java/kyonggiyo/domain/event/EventType.java
@@ -0,0 +1,9 @@
+package kyonggiyo.domain.event;
+
+public enum EventType {
+
+    IMAGE_CREATE,
+    IMAGE_UPDATE,
+    IMAGE_DELETE
+
+}
