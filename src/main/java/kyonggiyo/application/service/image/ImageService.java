bc6bcef363ed76ccb2712ffa6392874f0a32c213
weonest <geonhee33@naver.com>
2024-03-25 23:53:00 +0900

    :recycle: ImageService PresignedUrl 생성 및 이미지 삭제 기능 리팩토링

--- src/main/java/kyonggiyo/application/service/image/ImageService.java
+++ src/main/java/kyonggiyo/application/service/image/ImageService.java
@@ -1,6 +1,7 @@
 package kyonggiyo.application.service.image;
 
-import kyonggiyo.adapter.out.client.image.ImageUploader;
+import kyonggiyo.adapter.out.client.image.PresignedUrlProvider;
+import kyonggiyo.application.port.in.image.DeleteImageUseCase;
 import kyonggiyo.application.port.out.image.DeleteImagePort;
 import kyonggiyo.application.port.out.image.LoadImagePort;
 import kyonggiyo.application.port.out.image.SaveImagePort;
@@ -9,38 +10,42 @@ import kyonggiyo.domain.image.ImageType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ImageService {
+public class ImageService implements DeleteImageUseCase {
 
-    private final ImageUploader imageUploader;
+    private final PresignedUrlProvider presignedUrlProvider;
     private final SaveImagePort saveImagePort;
     private final LoadImagePort loadImagePort;
     private final DeleteImagePort deleteImagePort;
 
-    // TODO: 2024-03-06 ReviewCommandService의 트랜잭션에서 계속 이어지고 있음. 테스트 후에 분리해보기
     @Transactional
-    public void createImage(List<MultipartFile> multipartFiles, ImageType imageType, Long referenceId) {
-        List<String> keys = imageUploader.uploadImage(multipartFiles);
-        List<Image> images = keys.stream()
-                .map(v -> new Image(v, imageType, referenceId)).toList();
+    public void createImage(List<String> imageUrls, ImageType imageType, Long referenceId) {
+        List<Image> images = imageUrls.stream()
+                .map(v -> {
+                    String key = presignedUrlProvider.extractImageKey(v);
+                    return new Image(key, imageType, referenceId);
+                })
+                .toList();
         saveImagePort.saveAll(images);
     }
 
+    @Override
     @Transactional
-    public void deleteImage(ImageType imageType, Long referenceId) {
+    public void deleteById(Long id) {
+        deleteImagePort.deleteById(id);
+    }
+
+    @Transactional
+    public void deleteByImageTypeAndReferenceId(ImageType imageType, Long referenceId) {
         List<Image> images = loadImagePort.findAllByImageTypeAndReferenceId(imageType, referenceId);
         List<Long> ids = images.stream()
                 .map(Image::getId).toList();
 
         deleteImagePort.deleteAllByIdInBatch(ids);
-        for (Image image : images) {
-            imageUploader.deleteImage(image.getKey());
-        }
     }
 
 }
