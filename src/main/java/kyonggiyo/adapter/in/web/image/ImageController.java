040a8e7d01e40469984319de74378f9a39d6ea3e
weonest <geonhee33@naver.com>
2024-03-29 23:03:22 +0900

    :recycle: 이미지 컨트롤러 수정

--- src/main/java/kyonggiyo/adapter/in/web/image/ImageController.java
+++ src/main/java/kyonggiyo/adapter/in/web/image/ImageController.java
@@ -1,13 +1,13 @@
 package kyonggiyo.adapter.in.web.image;
 
+import kyonggiyo.adapter.in.web.image.dto.ImageDeleteRequest;
 import kyonggiyo.adapter.in.web.image.dto.PresignedUrlRequest;
 import kyonggiyo.adapter.in.web.image.dto.PresignedUrlResponse;
-import kyonggiyo.adapter.out.client.image.PresignedUrlProvider;
+import kyonggiyo.adapter.out.client.image.ImageManager;
 import kyonggiyo.application.port.in.image.DeleteImageUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/images")
 public class ImageController {
 
-    private final PresignedUrlProvider presignedUrlProvider;
+    private final ImageManager imageManager;
     private final DeleteImageUseCase deleteImageUseCase;
 
     @PostMapping("/presignedUrl")
     private ResponseEntity<PresignedUrlResponse> presignedUrlGenerate(@RequestBody PresignedUrlRequest request) {
-        String presignedUrl = presignedUrlProvider.generatePresignedUrl(request.filename());
+        String presignedUrl = imageManager.generatePresignedUrl(request.filename());
         return ResponseEntity.ok(new PresignedUrlResponse(presignedUrl));
     }
 
     @DeleteMapping("/{imageId}")
-    private ResponseEntity<Void> imageDelete(@PathVariable Long imageId) {
-        deleteImageUseCase.deleteById(imageId);
+    private ResponseEntity<Void> imageDelete(@RequestBody ImageDeleteRequest request) {
+        deleteImageUseCase.deleteById(request);
         return ResponseEntity.noContent().build();
     }
 
