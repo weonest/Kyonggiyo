66533a4dff9aaf147a8b58d4f689b24192aa3970
weonest <geonhee33@naver.com>
2024-03-29 23:02:34 +0900

    :sparkles: 이미지 삭제 Use Case 추가

--- src/main/java/kyonggiyo/application/port/in/image/DeleteImageUseCase.java
+++ src/main/java/kyonggiyo/application/port/in/image/DeleteImageUseCase.java
@@ -1,7 +1,9 @@
 package kyonggiyo.application.port.in.image;
 
+import kyonggiyo.adapter.in.web.image.dto.ImageDeleteRequest;
+
 public interface DeleteImageUseCase {
 
-    void deleteById(Long id);
+    void deleteById(ImageDeleteRequest request);
 
 }
