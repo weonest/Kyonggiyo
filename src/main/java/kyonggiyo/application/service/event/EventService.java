0413bd0a5cf42847669745ec682e26969e5f583d
weonest <geonhee33@naver.com>
2024-04-05 00:50:32 +0900

    :recycle: 이벤트 서비스 리팩토링 - 불필요한 로직 삭제 - Zero Payload 방식

--- src/main/java/kyonggiyo/application/service/event/EventService.java
+++ src/main/java/kyonggiyo/application/service/event/EventService.java
@@ -1,12 +1,8 @@
 package kyonggiyo.application.service.event;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import kyonggiyo.application.port.out.event.LoadEventPort;
 import kyonggiyo.application.port.out.event.SaveEventPort;
-import kyonggiyo.domain.event.EntityType;
 import kyonggiyo.domain.event.Event;
-import kyonggiyo.domain.event.EventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -16,9 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class EventService<T> {
+public class EventService {
 
-    private final ObjectMapper objectMapper;
     private final SaveEventPort saveEventPort;
     private final LoadEventPort loadEventPort;
 
@@ -31,26 +26,6 @@ public class EventService<T> {
     public void successEvent(Long id) {
         Event event = loadEventPort.getById(id);
         event.successEvent();
-    }
-
-    public String convertEventToJson(T event) {
-        try {
-            return objectMapper.writeValueAsString(event);
-        } catch (JsonProcessingException e) {
-            log.warn("이벤트를 JSON으로 변환 과정에서 예외가 발생하였습니다.");
-            throw new RuntimeException(e);
-        }
-    }
-
-    public T convertDataToEntity(Event event) {
-        String entityData = event.getEntityData();
-        EventType eventType = event.getEventType();
-        try {
-            return (T) objectMapper.readValue(entityData, eventType.getClazz());
-        } catch (JsonProcessingException e) {
-            log.warn("JSON을 엔티티로 변환하는 작업에서 예외가 발생하였습니다.");
-            throw new RuntimeException(e);
-        }
     }
 
 }
--- src/main/java/kyonggiyo/domain/event/EventType.java
+++ src/main/java/kyonggiyo/domain/event/EventType.java
@@ -1,20 +1,8 @@
 package kyonggiyo.domain.event;
 
-import kyonggiyo.application.service.event.ImageCreateEvent;
-
 public enum EventType {
 
-    IMAGE_CREATE(ImageCreateEvent.class),
+    IMAGE
     ;
-
-    public final Class clazz;
-
-    EventType(Class clazz) {
-        this.clazz = clazz;
-    }
-
-    public Class getClazz() {
-        return clazz;
-    }
 
 }
