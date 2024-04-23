b11e5d8e53c5678428041a343fecfe45a6395f61
weonest <geonhee33@naver.com>
2024-04-24 00:02:18 +0900

    :fire: ImageEvent Adapter Port 삭제

--- kyonggiyo-core/src/main/java/kyonggiyo/application/port/out/event/image/SaveImageEventPort.java
+++
@@ -1,9 +0,0 @@
-package kyonggiyo.application.port.out.event.image;
-
-import kyonggiyo.domain.event.ImageEvent;
-
-public interface SaveImageEventPort {
-
-    ImageEvent save(ImageEvent event);
-
-}
